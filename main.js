cbcd5a4c838c0a24570e11d46a52f8e850b3250c
narian18 <narianmoneaunaidu@gmail.com>
2024-04-03 17:11:01 +0200

    change path to self-reference

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { PuzzleGame } from "./puzzle-game";
 
     
-const game = new PuzzleGame('favicon.png', 5);
+const game = new PuzzleGame('./favicon.png', 5);
 
 window.addEventListener('mousedown', (event) => (game.onMouseDown(event)));
 window.addEventListener('dragend', (event) => (game.onDragEnd(event)));
